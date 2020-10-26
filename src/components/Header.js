dbdcf4a6f896576ff28aed143f140bcc866933cd
DinhSonHai <sdinhhai@gmail.com>
2020-10-26 21:27:58 +0700

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Header(props) {
+  return (
+    <div style={{ padding: '10px 50px', background: 'black' }}>
+      <div style={{ color: 'white' }}>Pizza Customization</div>
+    </div>
+  )
+};
+
+Header.propTypes = {
+
+};
+
+export default Header;
+
