8e2146b07b6ae331050e04c94121a303647a6a79
DinhSonHai <sdinhhai@gmail.com>
2020-10-29 21:13:29 +0700

    Show selected ingredients in checkout page

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Checkout({ ingredients }) {
   return (
-    <div>
-      { JSON.stringify(ingredients)}
+    <div style={{ display: 'flex' }}>
+      <div>
+        <h1>My Ingredients</h1>
+        { Object.keys(ingredients).map((ingredient) => {
+          return (ingredients[ingredient] && <p>{ ingredient[0].toUpperCase() }{ ingredient.substr(1) }</p> )
+        })}
+      </div>
     </div>
   )
 }
